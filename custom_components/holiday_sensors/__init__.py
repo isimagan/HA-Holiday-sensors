6be17ecefbbce7865afd1195fa151be6b27e59a7
Martin Gøranson <30817386+isimagan@users.noreply.github.com>
2026-09-13 02:04:36 +0200

    Set up Holiday Sensors config entries

--- custom_components/holiday_sensors/__init__.py
+++ custom_components/holiday_sensors/__init__.py
@@ -1,11 +1,17 @@
 """Holiday Sensors integration."""
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
 
-DOMAIN = "holiday_sensors"
+from .const import PLATFORMS
 
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up Holiday Sensors from YAML."""
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Holiday Sensors from a config entry."""
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a Holiday Sensors config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
